ef2fdaf129e1e35d7ff114901c2f806d49b8fa4f
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-09 11:27:36 -0500

    wraped up all component with Aux in layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Aux from '../../hoc/..';
 
 const layout = ( props ) => (
+    <Aux>
     <div>Toolbar, SideDrawer, BackDrop</div>
     <main>
         {props.children}
     </main>
+    </Aux>
 );
