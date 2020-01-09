01aa568e66fc6676ed020613c728a08e219889d4
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-09 15:08:59 -0500

    added BurgerBuilder component into App.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,8 +6,11 @@ class BurgerBuilder extends Component {
     render () {
         return(
             <Aux>
-                
+                <div>Burger</div>
+                <div>Build control</div>
             </Aux>
         );
     }
 }
+
+export default BurgerBuilder; 
