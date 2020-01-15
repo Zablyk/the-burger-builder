43bc2fd7ce97ae5564591e3c45e842d536528fb4
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-15 11:10:01 -0500

    added a global  const INGREDIENT_PRICES in to BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,15 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
+
+
 class BurgerBuilder extends Component {
     //    constructor(props) {
     //        super(props);
