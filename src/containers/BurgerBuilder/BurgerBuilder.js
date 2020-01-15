536d3a62d26d290331c663aff312c7f70a783a4c
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-15 14:35:52 -0500

    now we don't have a mistake when we are trying to do ingredients less then 0 and if it's o our less button is disabled

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -44,6 +44,9 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
@@ -56,13 +59,20 @@ class BurgerBuilder extends Component {
     }
 
     render () {
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
+        }
+        // we will get something like {salad: true, meat: false, ...}
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}    
-                    />
+                    disabled={disableInfo}/>
             </Aux>
         );
     }
