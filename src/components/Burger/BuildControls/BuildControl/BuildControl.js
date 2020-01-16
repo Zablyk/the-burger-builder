a5d8f0ccc384e8791cd3536c47ab0f9acbe5e5c3
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-16 08:58:55 -0500

    before adding a price everything work

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './BuildControl';
+import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
