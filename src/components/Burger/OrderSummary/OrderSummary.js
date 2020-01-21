ebcf595b2d85305504aaddb7b4b54f325de1c3f5
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-01-20 20:51:05 -0500

    adding OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
 
 const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map() 
     return (
         <Aux>
             <h3>Your order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-                
+
             </ul>
         </Aux>
     );
