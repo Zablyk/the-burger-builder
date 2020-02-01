6dd4a8db08c5172ddd482ced6a6f3d4b8668e036
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-02-01 16:10:17 -0500

    added a code in to purchaseContinueHandler in BurgerBuilder.js to post a order to our server

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-the-burger-builder-54abb.firebaseio.com/';
+    baseURL: 'https://react-the-burger-builder-54abb.firebaseio.com/'
 });
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,8 @@ import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -85,7 +86,28 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        //alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Oleg Zablotskyi',
+                address: {
+                    street: 'TestStreet',
+                    postalCode: 'L4X1T4',
+                    country: 'Canada'
+                },
+                email: 'test@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/order.json', order)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            });
     };
 
     render () {
