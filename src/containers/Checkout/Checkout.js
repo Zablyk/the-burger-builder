8c0b6646ec218f31ac2763d12c2f3db20706128d
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-03-03 20:43:56 -0500

    created ContactData component and will rout it in Checkout.js like nested Route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
+ 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
