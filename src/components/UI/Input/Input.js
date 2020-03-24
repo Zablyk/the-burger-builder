b8437d21f2154ad873570f040281c4507288ac11
Zablyk <zablotsky@gmail.com>
2020-03-24 14:55:37 -0400

    added a handler in to input.js and contactData we can save info from forms

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import classes from './Input.css'
@@ -10,19 +11,22 @@ const input = (props) => {
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case ('select'):
             inputElement = ( 
                 <select
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -35,7 +39,8 @@ const input = (props) => {
             inputElement = <input
                 className={classes.InputElement}
                 {...props.elementConfig}
-                value={props.value}/>;        
+                value={props.value}
+                onChange={props.changed}/>;        
     };
     return (
         <div className={classes.Input}>
