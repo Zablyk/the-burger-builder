16d3246db47afa05dd13855ce19afd2a671db904
Zablyk <zablotsky@gmail.com>
2020-04-14 18:55:42 -0400

    added a Redirect to put cade and know after order we will be redirected

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 import { connect} from 'react-redux';
 class Checkout extends Component {
-
+   
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
@@ -17,8 +17,10 @@ class Checkout extends Component {
     render() {
         let summary = <Redirect to="/"/>
         if  (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
+                {purchasedRedirect}
                 <CheckoutSummary 
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
@@ -34,8 +36,10 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
+
 
 export default connect(mapStateToProps)(Checkout);
