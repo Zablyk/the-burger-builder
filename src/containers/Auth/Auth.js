21e669adfa60f48f5b17027f4f76281820f481cd
Zablyk <zablotsky@gmail.com>
2020-04-15 22:21:57 -0400

    added Auth form in to  Auth.js and Auth folder in containers

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+    state = {
+       controls: {
+        email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Email Address',
+            },
+            value: '',
+            validation: {
+                required: true,
+                isEmail: true
+            },
+            valid: false,
+            touched: false
+        },
+        password: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'password',
+                placeholder: 'Password',
+            },
+            value: '',
+            validation: {
+                required: true,
+                minLength: 7,
+
+            },
+            valid: false,
+            touched: false
+        }
+    }
+}
+
+
+    render () {
+        const formElementArray = [];
+        for (let key in this.state.controls) {
+            formElementArray.push ({
+                id: key,
+                config: this.state.controls[key]
+            });
+        }
+
+        const form = formElementArray.map(formElement => (
+            <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                changed={(event) => this.inputChabgedHandler(event, formElement.id)}/>
+        ));
+
+        return (
+            <div>
+                <form>
+                    {form}
+                    <Button btnType="Success">SUBMIT</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
+
