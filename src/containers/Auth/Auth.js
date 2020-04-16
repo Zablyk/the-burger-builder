c802b158fb0c9e0a8397867ab6cd71389ca327a1
Zablyk <zablotsky@gmail.com>
2020-04-15 22:53:57 -0400

    added css classes and and validation to our Auth.js

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import classes from '../Auth/Auth.css';
 
 class Auth extends Component {
     state = {
@@ -38,6 +39,47 @@ class Auth extends Component {
     }
 }
 
+checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+    
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    return isValid;
+};
+
+inputChabgedHandler = (event, controlName) => {
+    const updatedControls = {
+        ...this.state.controls,
+        [controlName]: {
+            ...this.state.controls[controlName],
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            touched: true
+        }
+    };
+    this.setState({controls: updatedControls});
+}
+
 
     render () {
         const formElementArray = [];
@@ -61,7 +103,7 @@ class Auth extends Component {
         ));
 
         return (
-            <div>
+            <div className={classes.Auth}>
                 <form>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
