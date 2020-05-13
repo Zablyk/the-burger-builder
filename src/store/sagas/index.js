1626ecf06f4270afa7e98dc5844e82617dea8bbb
Zablyk <zablotsky@gmail.com>
2020-05-12 22:21:28 -0400

    moved auto-sigh-in from authCheckState store/actions/auth.js in to SAGA store/sagas/auth.js

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,11 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimoutSaga, authUserSaga} from './auth';
+import { logoutSaga, checkAuthTimoutSaga, authUserSaga, authCheckStateSaga} from './auth';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimoutSaga);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
